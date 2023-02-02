7985c452748822a5868e2c15fa6c37672c074532
alip4may <124228439+alip4may@users.noreply.github.com>
2023-02-02 11:11:04 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<h2 align="center">
+<hr>
+Autoscript SSH XRAYS Websocket Multiport (BETA)
+<h2><hr>
+  
+<h2 align="center"> ♦️Supported Linux Distribution♦️</h2>
+<p align="center"><img src="https://d33wubrfki0l68.cloudfront.net/5911c43be3b1da526ed609e9c55783d9d0f6b066/9858b/assets/img/debian-ubuntu-hover.png"width="400"></p>
+<p align="center"><img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%209&message=Stretch&color=purple"> <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%2010&message=Buster&color=purple">  <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=ubuntu&label=Ubuntu%2018&message=Lts&color=red"> <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=ubuntu&label=Ubuntu%2020&message=Lts&color=red">
+</p>
+
+## ⚠️ PLEASE README ⚠️
+
+
+ SILA PASTIKAN SETTING DOMAIN DI CLOUDFLARE ANDA SEPERTI DIBAWAH (SSL/TLS SETTINGS) <br>
+  1. Your SSL/TLS encryption mode is Full
+  2. Enable SSL/TLS Recommender ✅
+  3. Edge Certificates > Disable Always Use HTTPS (off)
+ 
+  
+  ## ⚠️   Register IP ⚠️
+  
+<a href="https://t.me/todfix667" target=”_blank”><img src="https://img.shields.io/static/v1?style=for-the-badge&logo=Telegram&label=Telegram&message=Click%20Here&color=blue"></a><br> 
+
+
+<br>
+</b>
+
+##  AUTOSCRIPT WEBSOCKET MULTIPORT DETAILS 
+
+<b>
+[ SSH & XRAY SERVICES ] <br>
+<br>
+✅ SSH WEBSOCKET TLS & NON-TLS <br>
+✅ XRAY VMESS WEBSOCKET TLS & NON-TLS <br>
+✅ XRAY VLESS WEBSOCKET TLS & NON-TLS <br>
+✅ XRAY TROJAN WEBSOCKET TLS & NON-TLS <br>
+✅ XRAY GRPC TLS <br>
+<br>
+[ OTHER SERVICES ] <br>
+<br>
+
+✅ BANDWITH MONITOR <br>
+✅ CHECK LOGIN USER <br>
+✅ CHECK CREATED CONFIG <br>
+✅ AUTOMATIC CLEAR LOG <br>
+✅ AUTOMATIC VPS REBOOT <br>
+✅ BACKUP & RESTORE <br>
+<br>
+<br>
+♦️ For Debian 10 Only For First Time Installation <br>
+<br>
+  
+  ```html
+apt update -y && apt upgrade -y && apt dist-upgrade -y && reboot
+  ```
+  
+♦️ For Ubuntu 18.04 Only For First Time Installation <br>
+  
+  ```html
+apt-get update && apt-get upgrade -y && apt dist-upgrade -y && update-grub && reboot
+ ```
+
+♦️ Installation Link<br>
+
+  ```html
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/NevermoreSSH/AutoScript-sshxraysws/main/setup.sh && chmod +x setup.sh && sed -i -e 's/\r$//' setup.sh && screen -S setup ./setup.sh
+```
+♦️ Custom SSH Websocket Banner (if you wanted)<br>
+
+  ```html
+cd /etc/
+nano issue.net
+```
+
+Menu & Port:
+
+<img src="https://github.com/NevermoreSSH/AutoScript-sshxraysws/blob/main/permission/image.png" width="900" title="Autoscript-Lite">
+</p>
+
---
+++ menu/menu.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo "Checking VPS"
+#########################
+IZIN=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/ipregnv/main/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+exit 0
+fi
+#EXPIRED
+expired=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/ipregnv/main/ip | grep $MYIP | awk '{print $3}')
+echo $expired > /root/expired.txt
+today=$(date -d +1day +%Y-%m-%d)
+while read expired
+do
+	exp=$(echo $expired | curl -sS https://raw.githubusercontent.com/NevermoreSSH/ipregnv/main/ip | grep $MYIP | awk '{print $3}')
+	if [[ $exp < $today ]]; then
+		Exp2="\033[1;31mExpired\033[0m"
+        else
+        Exp2=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/ipregnv/main/ip | grep $MYIP | awk '{print $3}')
+	fi
+done < /root/expired.txt
+rm /root/expired.txt
+Name=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/ipregnv/main/ip | grep $MYIP | awk '{print $2}')
+# Color Validation
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+cyan='\e[36m'
+Lred='\e[91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
+# VPS Information
+#Domain
+domain=$(cat /etc/xray/domain)
+#Status certificate
+modifyTime=$(stat $HOME/.acme.sh/${domain}_ecc/${domain}.key | sed -n '7,6p' | awk '{print $2" "$3" "$4" "$5}')
+modifyTime1=$(date +%s -d "${modifyTime}")
+currentTime=$(date +%s)
+stampDiff=$(expr ${currentTime} - ${modifyTime1})
+days=$(expr ${stampDiff} / 86400)
+remainingDays=$(expr 90 - ${days})
+tlsStatus=${remainingDays}
+if [[ ${remainingDays} -le 0 ]]; then
+	tlsStatus="expired"
+fi
+# OS Uptime
+uptime="$(uptime -p | cut -d " " -f 2-10)"
+# Download
+#Download/Upload today
+dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload yesterday
+dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload current month
+dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
+# Getting CPU Information
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
+cpu_usage+=" %"
+ISP=$(curl -s ipinfo.io/org?token=7578ac19afd785 | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city?token=7578ac19afd785 )
+WKT=$(curl -s ipinfo.io/timezone?token=7578ac19afd785 )
+DAY=$(date +%A)
+DATE=$(date +%m/%d/%Y)
+DATE2=$(date -R | cut -d " " -f -5)
+IPVPS=$(curl -s ipinfo.io/ip?token=7578ac19afd785 )
+cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
+tram=$( free -m | awk 'NR==2 {print $2}' )
+uram=$( free -m | awk 'NR==2 {print $3}' )
+fram=$( free -m | awk 'NR==2 {print $4}' )
+clear 
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "                 • SERVER INFORMATION •                 "
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[33m OS            \e[0m:  "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
+echo -e "\e[33m Kernel        \e[0m:  $(uname -r)"
+echo -e "\e[33m System Uptime \e[0m:  $uptime"		
+echo -e "\e[33m IP VPS        \e[0m:  $IPVPS"	
+echo -e "\e[33m Domain        \e[0m:  $domain"	
+echo -e "\e[33m Date & Time   \e[0m:  $DATE2"	
+echo -e "\e[33m RAM Usage     \e[0m:  $uram MB | $tram MB"	
+echo -e "\e[33m CPU Info      \e[0m:  $cpu_usage ($cname Processor @ $freq MHz)"	
+echo -e "\e[33m Bandwidth     \e[0m:  $ttoday (today) | $tmon (month)"
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "                 • MENU SSH | XRAYS •                 "
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e   ""
+echo -e " [\e[36m 01 \e[0m] Menu SSH Websocket" 
+echo -e " [\e[36m 02 \e[0m] Menu XRAY Vmess" 
+echo -e " [\e[36m 03 \e[0m] Menu XRAY Vless" 
+echo -e " [\e[36m 04 \e[0m] Menu XRAY Trojan"
+echo -e " [\e[36m 05 \e[0m] Menu Shadowsocks " 
+echo -e " [\e[36m 06 \e[0m] Menu VPS & System"
+echo -e " [\e[36m 07 \e[0m] Status Service"
+echo -e " [\e[36m 08 \e[0m] Clear RAM Cache"
+echo -e " [\e[36m 09 \e[0m] Check CPU & RAM"
+echo -e " [\e[36m 10 \e[0m] Check Bandwidth"
+echo -e   ""
+echo -e   " Press x or [ Ctrl+C ] • To-Exit-Script"
+echo -e   ""
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[33mClient Name   \E[1m: $Name"
+echo -e " \e[33mExpired       \E[1m: $Exp2"
+echo -e " \e[33mLicense Key   \E[1m: L67GT-CE6TR-DFT1D-XWVCM"
+echo -e " \e[33mVersion       \E[1m: SSH XRAYS WS Beta v0.3"
+echo -e " \e[33mAutoScript    \E[1m: Multiport Websocket By NevermoreSSH"
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e   ""
+read -p " Select menu :  "  opt
+echo -e   ""
+case $opt in
+1) clear ; m-sshovpn ;;
+2) clear ; m-vmess ;;
+3) clear ; m-vless ;;
+4) clear ; m-trojan ;;
+5) clear ; m-ssws ;;
+6) clear ; m-system ;;
+7) clear ; running ;;
+8) clear ; clearcache ;;
+9) clear ; htop ;;
+10) clear ; vnstat ;;
+x) exit ;;
+*) echo "Wrong Number Key " ; sleep 1 ; menu ;;
+esac
