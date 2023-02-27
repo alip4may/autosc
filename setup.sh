83ef345e245090b3761ad298f164e60e766f7680
alip4may <124228439+alip4may@users.noreply.github.com>
2023-02-28 05:37:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -106,24 +106,7 @@ echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
 sleep 0.5
 echo -ne "[ ${green}INFO${NC} ] Check permission : "
 
-PERMISSION
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-green "Permission Accepted!"
-else
-red "Permission Denied!"
-rm setup.sh > /dev/null 2>&1
-sleep 0.5
-exit 0
-fi
-sleep 0.5
 
-mkdir -p /var/lib/ >/dev/null 2>&1
-echo "IP=" >> /var/lib/ipvps.conf
-
-echo ""
 #wget -q https://raw.githubusercontent.com/alip4may/autosc/main/tools.sh;chmod +x tools.sh;./tools.sh
 #rm tools.sh
 clear
